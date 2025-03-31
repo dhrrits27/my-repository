f21dc90dba900fe4a14c8f1f788043ce0d55b348
dhrrits27 <dhrrits27@gmail.com>
2025-03-31 10:45:46 +0530

    Add files via upload

---
+++ Dhriti coding/print _statements.py
@@ -0,0 +1,21 @@
+print("Welcome to programming")
+
+print(5)
+
+print("Hello python \n")
+
+print("Python",3.5)
+
+print("Welcome to",end="*")
+
+name=input("Enter your name: ")
+print("Hello",name,"\n Welcome to Codingal")
+
+print("Hello {0} Nice to meet you {1}".format("dear",name))
+
+print("\a")
+
+print(f"Nice to meet u {name}")
+
+import keyword
+print(keyword.kwlist)
