192479f2e8a34dd896618e8f8f6766382c11eb3c
dhrrits27 <dhrrits27@gmail.com>
2025-04-01 10:45:02 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,46 @@
+a=5
+print("types of a: ",type(a))
+
+b=2.5
+print("type of b: ",type(b))
+
+c="coding"
+print("type of c: ",type(c))
+
+d=True
+print("type of d: ",type(d))
+
+
+name="Dhriti"
+age=17
+is_student=True
+weight=42.5
+
+print("Name: ",name)
+print("Data type of name is: ",type(name))
+
+print("age: ",age)
+print("data type of age is: ",type(age))
+
+print("is_student ",is_student)
+print("data type of is_student is: ",type(is_student))
+
+print("weight is: ",weight)
+print("data type of weight is: ",type(weight))
+
+weight=int(weight)
+print("weight is: ",weight,type(weight))
+
+
+a=2
+b=7
+print("a+b= ",str(a)+str(b))
+text=input("Enter a string: ")
+revtext=text[::-1]
+print("reverse of given string is: ")
+print(revtext)
+
+name=input("enter yor name: ")
+sname=input("enter your surname: ")
+fullname=name+" "+sname
+print("my name is: ",fullname)
