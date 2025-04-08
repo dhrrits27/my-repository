ec861716d69ca73eed81b4e02e7027973360e09f
dhrrits27 <dhrrits27@gmail.com>
2025-04-08 10:45:45 +0530

    Add files via upload

---
+++ and.py
@@ -0,0 +1,7 @@
+a=12
+b=10
+c=-1
+if a and b and c:
+    print("all the numbers hv boolean value as true")
+else:
+    print("atleast one number has boolean value as false")
---
+++ ope.py
@@ -0,0 +1,6 @@
+a="python"
+b="coding"
+if a!=b:
+    print(a,"and",b,"are different")
+else:
+    print(a,"and",b,"are same")
