63f83980a582161707018f3ba0b028c7550c31f6
dhrrits27 <dhrrits27@gmail.com>
2025-04-14 10:47:18 +0530

    Add files via upload

---
+++ Correction.py
@@ -0,0 +1,12 @@
+mean1=38
+wrong_num=36
+correct_num=56
+total_num=40
+sum=mean1*total_num
+print("the sum of 40 nos: ",sum)
+
+num2=sum-((wrong_num)-(correct_num))
+print("sum-((wrong_num)-(correct_num)): ",num2)
+
+mean2=num2/total_num
+print(mean2)
---
+++ Operator_Precedence.py
@@ -0,0 +1,15 @@
+v=7
+w=5
+x=9
+y=3
+z= (v+x)* w % y
+print("value of z is: ",z)
+
+
+name="alex"
+age="0"
+
+if (name=="alex" or name=="john" and age>="2"):
+    print("hello")
+else:
+    print("bye")
