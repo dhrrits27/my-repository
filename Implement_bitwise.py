d858e14d0775d0e3a5e2af13327f8d9082c15f81
dhrrits27 <dhrrits27@gmail.com>
2025-07-22 10:59:35 +0530

    Add files via upload

---
+++ Implement_bitwise.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 4
+
+print("num1 & num2 = ", num1 & num2)
+
+print("\n num1|num2 = ", num1|num2)
+
+print("\n ~num2 = ", ~num2)
+
+print("\n num1^num2 = ", num1^num2) 
+
+print("\n num2>>1 = ", num2>>1)
+
+print("\n num1<<1 = ", num1<<1)
+
+print("\n num2<<2 = ", num2<<2)
---
+++ Odd_Even_Bitwise.py
@@ -0,0 +1,6 @@
+n = 10
+
+if (n^1 == n+1):
+    print("the number is even")
+else:
+    print("the number is odd")
